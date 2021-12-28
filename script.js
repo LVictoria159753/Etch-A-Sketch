df3ee008af2b6d5ca09e7039daa2ef8e8a4193ca
Victoria <Lvictoria159753@gmail.com>
2021-12-27 23:46:17 -0800

    div 3x3

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 //Created one div and styled it
-let div= document.createElement("div");
-let div2= document.createElement("div");
-let div3= document.createElement("div");
+/*let div= document.createElement("div");
 document.getElementById("mini").append(div);
-document.getElementById("div").appendChild(div2)
+
 div.style.width="50px";
 div.style.height="50px";
 div.style.color= "green";
 div.style.background="red";
 div.innerHTML = "Testing";
+*/
 
+/*
+function divBlock(grid){
+for (let i=0; i<grid ;i++){
+    let row= document.getElementById("mini").append(div);
+    container.appendChild(row);
+        row.style.width="50px";
+        row.style.height="50px";
+        row.style.color= "green";
+        row.style.background="red";
+        row.innerHTML = "Testing";
 
-let array= [];
-function divBlock(){
-for (let i=0; i<257 ;i++){
-    array[i]= div;
-    document.getElementById("container").append(array[i]);
 }
 }
 
-divBlock();
+divBlock(100);
+*/
 
+
+function divBlock() {
+    for (let i=0; i<3;i++){
+        let row= document.createElement('div');
+        row.className = "row";
+     for (let j=0; j<3 ;j++){
+        let box = document.createElement('div');
+               box.className = "box";
+               row.appendChild(box);
+    }
+        document.getElementById('container').appendChild(row);
+    
+    }
+    }
+    divBlock();
+    
+    
+    
+    
+    
